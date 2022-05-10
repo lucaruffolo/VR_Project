9bbeb408025e006d248441f41b76166565adb8c0
Luca <tbocyber@gmail.com>
2022-05-10 12:44:43 +0200

    update physics

---
+++ Assets/Script/carController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carController : MonoBehaviour
+{
+    public WheelCollider[] wheels;
+    public float motorPower = 100;
+    public float steerPower = 100;
+
+    public GameObject centerOfMass;
+    public Rigidbody rb;
+
+    public GameObject leftWheel;
+    public GameObject rightWheel;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = centerOfMass.transform.localPosition;
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        //leftWheel.GetComponent<Transform>().rotation = new Quaternion(0, 0, 0, 1);
+        //rightWheel.GetComponent<Transform>().rotation = new Quaternion(0, 0, 0, 1);
+
+        foreach (var wheel in wheels) {
+            wheel.motorTorque = Input.GetAxis("Vertical") * ((motorPower * 5) / 4);
+
+        }
+
+        for (int i = 0; i < wheels.Length; i++) {
+            if (i < 2) {
+                wheels[i].steerAngle = Input.GetAxis("Horizontal") * steerPower;                
+            }
+        }
+    }
+}
