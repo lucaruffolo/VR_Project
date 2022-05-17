90c611b33154aa881ac44df1c938c7d0c52375a1
PierpaoloLopez <lpzppl98m18d086j@studenti.unical.it>
2022-05-17 11:08:36 +0200

    inserito timer gara

--- Assets/Script/TimerScript.cs
+++ Assets/Script/TimerScript.cs
@@ -6,24 +6,34 @@ using UnityEngine.UI;
 public class TimerScript : MonoBehaviour
 {
     public Text timer;
+    public Text timeRace;
     private float startTime;
+    private float startTimeRace;
     private bool finishSession = false;
     // Start is called before the first frame update
     void Start()
     {
-        startTime = 10.00f; //10 min = 600.00f
+        startTime = 20.00f; //10 min = 600.00f
+        startTimeRace = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //timer sessione
         float t = startTime - Time.time;
         if (!finishSession)
         {
             string minutes = ((int)t / 60).ToString();
-            string seconds = (t % 60).ToString("f2");
+            string seconds = (t % 60).ToString("f0");
             //Debug.Log(seconds);
-            timer.text = minutes + ":" + seconds;
+            if((t % 60) <= 9)
+            {
+                timer.text = minutes + ":0" + seconds;
+            }
+            else
+                timer.text = minutes + ":" + seconds;
+            
 
             if (((int)t / 60) <= 3)
             {
@@ -35,7 +45,17 @@ public class TimerScript : MonoBehaviour
         {
             finishSession = true;
             Destroy(GetComponent<TimerScript>().timer);
+            GetComponent<TimerScript>().timer = null;
+            
+            Destroy(GetComponent<TimerScript>().timeRace);
+            GetComponent<TimerScript>().timeRace = null;
             //Debug.Log("fine");
         }
+       
+        //timer gara
+        float delta = Time.time - startTimeRace;
+        string minutesRace = ((int)delta / 60).ToString();
+        string secondsRace = (delta % 60).ToString("f2");
+        timeRace.text = minutesRace + ":" + secondsRace;
     }
 }
