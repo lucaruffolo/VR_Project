ff1074bad3c104bbeb459cc13a71992cbfd6e427
PierpaoloLopez <lpzppl98m18d086j@studenti.unical.it>
2022-07-12 18:02:07 +0200

    3..2..1..VIA

---
+++ Assets/Script/StartingCountDown.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+
+public class StartingCountDown : NetworkBehaviour
+{
+    public float timeLeft;
+    public bool timerOn = false;
+    [SerializeField] Text countDown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeft = 3.0f;
+        timerOn = true;
+        countDown.enabled = true;
+        GetComponent<carController>().enabledMovement = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (this.isLocalPlayer)
+        //{
+        if (timerOn)
+        {
+            if (timeLeft > 0)
+            {
+                countDown.enabled = true;
+                if(timeLeft < 0.99)
+                {
+                    countDown.text = "VIA";
+                }
+                else
+                {
+                    countDown.text = timeLeft.ToString("f0");
+                }  
+                timeLeft -= Time.deltaTime;
+            }
+            else
+            {
+                Debug.Log("Fine tempo");
+                timeLeft = 0;
+                timerOn = false;
+                GetComponent<carController>().enabledMovement = true;
+                countDown.enabled = false;
+            }
+        }
+        //} 
+    }
+}
