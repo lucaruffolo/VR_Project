185c13b8b455de7cd44f85f579dad82bd73f5d71
PierpaoloLopez <lpzppl98m18d086j@studenti.unical.it>
2022-07-17 01:08:15 +0200

    nil2

---
+++ Assets/Script/StartLine.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartLine : MonoBehaviour
+{
+    public List<GameObject> listPlayer;
+    private bool yetInGame = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject player = other.gameObject.transform.parent.transform.parent.gameObject;
+        
+        foreach(GameObject i in listPlayer)
+        {
+            if (i == player)
+                yetInGame = true;
+        }
+
+        if (!yetInGame)
+        {
+            listPlayer.Add(player);
+            player.GetComponent<gameControl>().PlayerName = PlayerPrefs.GetString("namePlayer");
+
+            
+        }
+           
+
+        /*foreach (GameObject i in listPlayer)
+        {
+            Debug.Log(player.GetComponent<gameControl>().PlayerName);
+            Debug.Log(PlayerPrefs.GetString("namePlayer"));
+        }*/
+    }
+}
