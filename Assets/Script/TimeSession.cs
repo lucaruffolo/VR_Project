f9220c585952ecb454f2fbe16a142c1cf2a8b443
Luca <tbocyber@gmail.com>
2022-07-23 12:19:24 +0200

    Fine

--- Assets/Script/TimeSession.cs
+++ Assets/Script/TimeSession.cs
@@ -17,7 +17,7 @@ public class TimeSession : MonoBehaviour
         sessionReady = false;
         timerIsRunning = true;
         timeSession.enabled = true;
-        sessionTime = (3)*60.0f;
+        sessionTime = (10)*60.0f;
     }
     void Update()
     {
